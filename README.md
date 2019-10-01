3a7410aa63bbc1fdc9f40b19604b4ed6896d596e
Dave Hong <liangcai.stdnt@gmail.com>
2019-10-01 21:32:02 +0700

    Added Rooms.

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
-Chattr
+# Chattr
+#### A socket.io powered chatting application.
+
+## Setting Up...
+1. You will need nodejs to be installed in your computer. Download the latest version [here](https://www.nodejs.org/en/download).
+2. After you have downloaded and installed nodejs, clone this repository (or download) and place it somewhere nice.
+3. Open that folder hold shift and right click on an empty space.
+4. Click the "Open PowerShell window here" button. A Windows PowerShell terminal will pop up in that directory.
+5. Type in the following command `node ./index.js` and hit enter.
+6. Wait until there is an output saying: `listening on *:3000`.
+7. Done. The server is up and running.
+
+## Connecting to the server...
+1. Open your preferred browser.
+2. Type in `localhost:3000` in the url text bar and hit enter.
+3. Done. You are connected to Chattr. Feel free to explore this simple application.
+
+## Disclaimer
+> THIS APPLICATION IS A WORK IN PROGRESS!
--- index.js
+++ index.js
@@ -14,21 +14,32 @@ app.get('/chattr.html', function(req, res) {
 // If user connects
 io.on('connection', function(socket) {
   socket.username = "noname";
+  socket.room = "world";
+  socket.join("world");
 
   // If user sends a message
   socket.on('chat message', function(msg) {
-    io.emit('chat message', socket.username, msg);
+    io.in(socket.room).emit('chat message', socket.username, msg);
   });
 
   // If user changes username
   socket.on('username', function(username) {
-    io.emit('username change', socket.username, username);
+    io.in(socket.room).emit('username change', socket.username, username);
     socket.username = username;
   });
 
   // If user disconnects
   socket.on('disconnect', function() {
     io.emit('client disconnect', socket.username);
+  });
+
+  // Change room
+  socket.on('change room', function(roomID) {
+    socket.to(socket.room).emit('room leave', socket.username);
+    socket.room = roomID;
+    socket.leaveAll();
+    socket.join(socket.room);
+    socket.to(socket.room).emit('room join', socket.username);
   });
 });
 
